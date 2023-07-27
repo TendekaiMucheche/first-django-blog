c42a90cce235bee0dd14b0099cf9fd226670fc3d
TendekaiMucheche <tendekai50@gmail.com>
2023-07-27 17:06:23 +0200

    Created and Changed the HTML for the blog

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+# from django.conf.urls import url
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    ]
+
+# urlpatterns = [
+#     url(r'^$', views.post_list, name='post_list'),
+#     ]
